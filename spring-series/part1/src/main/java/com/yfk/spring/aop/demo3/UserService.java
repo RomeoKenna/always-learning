35902461d9627822bc0407fcc6158fa03390bc8f
yufukang <erkang0816@qq.com>
2022-08-26 09:43:57 +0800

    demo3,4,5

---
+++ spring-series/part1/src/main/java/com/yfk/spring/aop/demo3/UserService.java
@@ -0,0 +1,7 @@
+package com.yfk.spring.aop.demo3;
+
+public class UserService {
+    public void work(String userName) {
+        System.out.println(userName + ",正在和路人甲java一起学Spring Aop，欢迎大家一起来！");
+    }
+}
