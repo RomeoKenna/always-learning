6d0006c9721b3eb1431ad1e0c3e8800beafc1d79
yufukang <erkang0816@qq.com>
2022-09-01 21:30:42 +0800

    async demo 123

---
+++ spring-series/part1/src/main/java/com/yfk/spring/async/demo1/MainConfig1.java
@@ -0,0 +1,9 @@
+package com.yfk.spring.async.demo1;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@ComponentScan
+@EnableAsync
+public class MainConfig1 {
+}
